9cfdff2bf4fc16949877f6c1ade102269472cb48
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2025-11-10 19:42:29 +0530

    Add files via upload

---
+++ Percentage Calculation.py
@@ -0,0 +1,6 @@
+maths = int(input("Marks scored in maths out of 100 : "))
+science = int(input("Marks scored in science out of 100 : "))
+hindi = int(input("Marks scored in hindi out of 100 : "))
+computers = int(input("Marks scored in computers out of 100 : "))
+average_percentage = ((maths+science+hindi+computers)/400)*100
+print("The average marks of all 4 subjects out of 100 is", average_percentage)
