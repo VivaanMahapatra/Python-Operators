92358be60734ec45e8badadf6044d9ed44962dc4
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2025-11-20 19:45:48 +0530

    Add files via upload

---
+++ ACP_checking_if_alphabet.py
@@ -0,0 +1,5 @@
+character = input("please enter a character on your keyboard : ")
+if character.isalpha():
+    print("The character is an alphabet")
+else:
+    print("The character is not an alphabet, please enter an alphabet")
---
+++ Leap_Year.py
@@ -0,0 +1,5 @@
+year = int(input("Enter the year you want to check whetehr it is a leap year or not"))
+if year/4==0:
+    print("it is a leap year")
+else:
+    print("it is not a leap year")
