b6357417273524deac7dfda6f0f50b106c931c1c
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2025-12-20 17:17:39 +0530

    Add files via upload

---
+++ Number_Swap.py
@@ -0,0 +1,10 @@
+a1 = int(input("Please enter the value of 1st number : "))
+b1 = int(input("Please enter the value of 2nd number : "))
+c1 = int(input("Please enter the value of 3rd number : "))
+a=0
+b=0
+c=0
+a=b1
+b=c1
+c=a1
+print("the new swapped values of the numbers are, ", a, b, c)
