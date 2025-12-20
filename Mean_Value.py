85c682f6b35e373aa61d131f7a84730a189d594b
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2025-12-20 17:03:00 +0530

    Add files via upload

---
+++ Mean_Value.py
@@ -0,0 +1,11 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+
+sum = mean1 * total_number
+print("the sum of 40 number: ", sum)
+sum2 = sum - ((wrong_number) - (correct_number))
+print("sum-((wrong_number)-(correct_number)): ", sum2)
+mean2 = sum2 / total_number
+print(mean2)
