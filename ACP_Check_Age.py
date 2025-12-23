f14049b0e70705ed7c22df0e310ea6d640f8e485
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2025-12-23 20:01:20 +0530

    Add files via upload

---
+++ ACP_Check_Age.py
@@ -0,0 +1,8 @@
+age = int(input("Please enter your age in years : "))
+if age>=10:
+    if age<=20:
+        print("You are aged between 10 and 20, (this matches the criteria the program is asking for)")
+    else:
+        print("You are aged over 20, (this does not match the criteria the program is asking for)")
+else:
+    print("You are aged less than 10, (this does not match the criteria the program is asking for)")  2
