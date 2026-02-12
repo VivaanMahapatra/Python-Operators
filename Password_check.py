0a9636330670ff83cefbbf24bce14eab81ff0bd5
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2026-02-12 19:34:44 +0530

    Add files via upload

---
+++ Password_check.py
@@ -0,0 +1,7 @@
+while True:
+    password=input("Please enter the password : ")
+    if password=="Codingal123":
+        print("Access granted")
+        break
+    else:
+        print("Wrong password, try again.")
