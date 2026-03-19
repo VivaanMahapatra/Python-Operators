97a5b9268e6ef47ee4031cc3747afc5f8cdde682
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2026-03-19 19:55:07 +0530

    Add files via upload

---
+++ list_play.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30, 40, 50]
+total = sum(numbers)
+average = total / len(numbers)
+largest = max(numbers)
+smallest = min(numbers)
+print("Sum:", total)
+print("Average:", average)
+print("Largest number:", largest)
+print("Smallest number:", smallest)
