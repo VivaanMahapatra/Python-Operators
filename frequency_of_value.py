ebe9c371ee24bb7a75e12a8cf4b736c09c08a2d7
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2026-04-09 19:35:34 +0530

    Add files via upload

---
+++ frequency_of_value.py
@@ -0,0 +1,10 @@
+test_dict = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' :  2, 'coding' : 1}
+print("The original dictionary : "+ str(test_dict))
+K=2
+res=0
+for value in test_dict:
+    if test_dict[value]==K:
+        res+=1
+print("Frequency of K is : "+str(res))
+
+      
