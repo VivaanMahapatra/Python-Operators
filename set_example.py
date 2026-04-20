6a5075441498730180f0ae6e4a1aca8427011774
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2026-04-20 19:04:45 +0530

    Add files via upload

---
+++ set_example.py
@@ -0,0 +1,17 @@
+my_set = {1,2,3}
+print(my_set)
+
+my_set = {1.0,'Hello',(1,2,3,)}
+print(my_set)
+
+my_set={1,5,2,4,3,6}
+print(my_set)
+
+my_set={1,2,3,2}
+print(my_set,"\n")
+
+num_set = set([0,1,2,3,4,5])
+print("Original set:")
+print(num_set)
+num_set.pop()
+print(num_set)
