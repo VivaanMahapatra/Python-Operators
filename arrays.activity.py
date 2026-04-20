6d3fb9a13980d92ec4eeeaba23b16c28031ea7f9
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2026-04-20 19:27:00 +0530

    Add files via upload

--- arrays.activity.py
+++ arrays.activity.py
@@ -1,22 +1,8 @@
 import array as arr
-a = arr.array('i',[1,2,3])
-print("\nThe new array created is : ", end="")
-for i in range (0,3):
-    print(a[i], end = "")
-print()
-
-b = arr.array('d', [2.5,3.2,3.3])
-print("\nThe new created array is : ",end="")
-for i in range (0,3):
-    print(b[i],end="")
-a.insert(1,4)
-print("\n array after intersection : ", end="")
-for i in range (a):
-    print (i,end="")
-print()
-b.append(4.4)
-for i in range (b):
-    print (i,end="")
-print()
-print("access elemnt is : ",a[2])
-print("access elemnt is : ",b[0])
+array_num = arr.array('i',[1,3,5,7,9,3])
+print("Original array :"+str(array_num))
+print("Number of occurences of number 3 in said array is : "
++str(array_num.count(3)))
+array_num.reverse()
+print("The reverse order of the numbers in the array : ")
+print(str(array_num))
